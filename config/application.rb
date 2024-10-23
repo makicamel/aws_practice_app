8878e52892c0c028798331a86a1ea1a5bee4a9df
makicamel <unright@gmail.com>
2024-10-24 05:52:16 +0900

    Add TodoUploader

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module AwsPracticeApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('app/uploaders')
 
     # Don't generate system test files.
     config.generators.system_tests = nil
